872c38931fb2fd5a5802b687084a7c2ea836501c
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-12-03 17:24:13 -0600

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Renato Oliveira Fraga on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
